2679ba0083b2bda4f4ee8652ec7e7a1d52fb03e4
Katerina Georgiou <katerinaldg@gmail.com>
2018-04-26 15:01:55 +0100

    Initial setup

---
+++ src/main/java/game/GameRunner.java
@@ -0,0 +1,17 @@
+package game;
+
+import game.Board;
+import game.Game;
+
+public class GameRunner {
+
+    public static void main(String[] args) {
+        CommandLineUI inputOutput = new CommandLineUI(System.out, System.in);
+        inputOutput.askForBoardSize();
+        int boardSize = inputOutput.getBoardSize();
+        Board board = new Board(boardSize);
+        Game game = new Game(inputOutput, board);
+
+        game.run();
+    }
+}
